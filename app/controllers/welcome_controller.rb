1aa43a916608ac9f61d1ee5f22ac649f7113779d
AlexPan <ppgod@live.cn>
2017-10-10 16:28:44 +0800

    add bootstrap flash function

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
 class WelcomeController < ApplicationController
+
+  def index
+    flash[:notice] = "This is a notice!"
+    flash[:alert] = "This is a alert!"
+    flash[:warning] = "This is a warning!"
+  end
 end
